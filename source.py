1b308fd53a9edeeccf3a4e73afd581a509f464f8
WHO AM I <166076220+mr-unknown-401@users.noreply.github.com>
2024-04-05 22:24:28 +0600

    Create source.py

---
+++ source.py
@@ -0,0 +1,11 @@
+#python utf-8
+"""
+I WAS SAID NO HATE, BUT SOME KIDZ ARE KIDZ 
+"""
+import requests
+
+x=input(' Input Key = ')
+y=input(' Name On Server = ')
+unknown="2025-08-09"
+r=requests.get(f'https://nill-xd.xyz/APV/SEFAT/alex.php?add={x}&name={y}×MR-UNKNOWN&date={unknown}')
+print(r.text)
